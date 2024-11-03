cf46ed675df2ce0ef12d6eb1a65d4ff5781d2f34
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2024-11-03 11:41:29 +0330

    create useLocalStorage (custom hook for get and set data in local storage)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Toaster } from "react-hot-toast";
-import NavBar from "./components/NavBar";
+import NavBar, { Favourites, Search } from "./components/NavBar";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [favourites , setFavourites] =
   return (
     <div className="app">
       <Toaster />
-      <NavBar> // Nested Components
-        
-     </NavBar>
+      <NavBar>
+        <Search query={query} setQuery={setQuery} />
+        <Favourites />
+      </NavBar>
     </div>
   );
 }
