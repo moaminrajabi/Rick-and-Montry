b00747f96cfc00f2af35716de32006efdf172432
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2024-11-03 11:49:25 +0330

    create Loader component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Toaster } from "react-hot-toast";
 import NavBar, { Favourites, Search } from "./components/NavBar";
+import useLocalStorage from "./hooks/useLocalStorage";
 
 function App() {
   const [query, setQuery] = useState("");
-  const [favourites , setFavourites] =
+  const [selectedId, setSelectedId] = useState(null);
+  const [favourites, setFavourites] = useLocalStorage("FAVOURITES", []);
+
+  useEffect(() => {
+    const interval = setInterval(() => setCount((c) => c + 1), 1000);
+    // return function(){}
+    return () => {
+      clearInterval(interval);
+    };
+  }, [count]);
+
+  const handleSelectCharacter = (id) => {
+    setSelectedId((prevId) => (prevId === id ? null : id));
+  };
+
+  const handleDeleteFavourite = (id) => {
+    setFavourites((prevFav) =>
+      prevFav.filter((fav) => fav.id).includes(selectedId)
+    );
+  };
+
   return (
     <div className="app">
       <Toaster />
       <NavBar>
         <Search query={query} setQuery={setQuery} />
-        <Favourites />
+        <Favourites
+          favourites={favourites}
+          onDeleteFavourite={handleDeleteFavourite}
+        />
       </NavBar>
+      <Main>
+        
+      </Main>
     </div>
   );
 }
